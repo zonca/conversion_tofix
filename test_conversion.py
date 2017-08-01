a95e524610d2367409032166e200ac308fcb5071
Andrea Zonca <code@andreazonca.com>
2017-08-01 08:49:11 -0700

    test gallons 2 liters

--- test_conversion.py
+++ test_conversion.py
@@ -3,3 +3,7 @@ import conversion
 assert conversion.dollars2cents(1) == 100
 assert conversion.dollars2cents(.1) == 10
 assert conversion.dollars2cents(0) == 0
+
+assert conversion.gallons2liters(1) == 3.78541
+assert conversion.gallons2liters(2) == 7.57082
+assert conversion.gallons2liters(0) == 0
