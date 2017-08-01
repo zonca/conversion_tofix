7ae6fb18a1ad9ad43d88ff84c67e95db89a6227d
Benjamin Fildier <benjamin.fildier@gmail.com>
2017-08-01 13:27:00 -0700

    Revert "add docstrings"
    This reverts commit 81c6aecffa27183d4f504648a79a3ce6f6328253.

--- conversion.py
+++ conversion.py
@@ -1,9 +1,7 @@
 def dollars2cents(dollars):
-    """Convert dollars to cents"""
     cents = dollars * 100
     return cents
 
 def gallons2liters(gallons):
-    """Convert gallons to liters"""
     liters = gallons * 3.78541
     return liters
