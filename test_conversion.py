188711092dae9cc472b6d7714ebdc69b677a81c6
Andrea Zonca <code@andreazonca.com>
2017-08-01 08:59:20 -0700

    inform that testing is finished

--- test_conversion.py
+++ test_conversion.py
@@ -7,3 +7,5 @@ assert conversion.dollars2cents(0) == 0
 assert conversion.gallons2liters(1) == 3.78541
 assert conversion.gallons2liters(2) == 7.57082
 assert conversion.gallons2liters(0) == 0
+
+print("Testing completed")
